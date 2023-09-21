9e3940d22c8e6d4c12e9de5ceb6100eb6224fedf
Christoffer Agger <christoffer.agger@gmail.com>
2023-09-21 18:01:27 +0200

    adding chances before short break

--- client/components/chat.tsx
+++ client/components/chat.tsx
@@ -4,8 +4,26 @@ const Chat = ({ chat }) => {
 
     console.log(chat);
     return (
-        <div className='w-1/2 ml-auto overflow-auto border pt-14'>
-            <p>this is a chat</p>
+        <div className='w-1/2 ml-auto overflow-auto border pt-14 pb-24'>
+            {Array.isArray(chat) && chat.length > 0 ? (
+                chat.map((entry, index) => {
+                    const query = entry.query;
+                    const response = entry.response;
+
+                    return (
+                        <div key={index}>
+                            <div>
+                                <p>{query}</p>
+                            </div>
+                            <div>
+                                <p>{response}</p>
+                            </div>
+                        </div>
+                    )
+                })
+                ) : (
+                    <p>Let's get started</p>
+                )}
         </div>
     )
 }
