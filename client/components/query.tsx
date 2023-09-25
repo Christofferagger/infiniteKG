82e81bc78ee9ebcc4605b82437008648dc543172
Christoffer Agger <christoffer.agger@gmail.com>
2023-09-25 21:11:45 +0200

    UX improvement and loading

--- client/components/query.tsx
+++ client/components/query.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState, useRef } from 'react'
-import autosize from 'autosize'
+import React, { useEffect, useState, useRef } from 'react';
+import autosize from 'autosize';
 
 const Query = ({ setGraphData, setChat, setIsChatVisible }) => {
 
@@ -7,6 +7,25 @@ const Query = ({ setGraphData, setChat, setIsChatVisible }) => {
     const [buttonClicked, setButtonClicked] = useState('Graph');
     const [knowledgeGraph, setKnowledgeGraph] = useState(null); 
     const [isLoading, setIsLoading] = useState(false); 
+    const [loadingMessage, setLoadingMessage] = useState('Processing...');
+    const [opacity, setOpacity] = useState(1);
+
+    const loadingMessages = ['Processing...', 'Still working...', 'Almost done...', 'Purifying...', 'Another word for loading...', 'Patience is the road to wisdom...', "Chill dude it's free"];
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            if (isLoading) {
+                setOpacity(0);
+                setTimeout(() => {
+                    const randomIndex = Math.floor(Math.random() * loadingMessages.length);
+                    setLoadingMessage(loadingMessages[randomIndex]);
+                    setOpacity(1);
+                }, 500); 
+            }
+        }, 2500);
+    
+        return () => clearInterval(intervalId); 
+    }, [isLoading]);
 
     const handleSubmit = () => {
         setIsLoading(true);
@@ -34,27 +53,23 @@ const Query = ({ setGraphData, setChat, setIsChatVisible }) => {
     }, []);
 
     return (
-        <div className='fixed bottom-0 inset-x-0 w-full flex items-center justify-center px-72 py-6 space-x-4 z-10 border-t bg-white-custom'>
+        <div className='fixed bottom-0 inset-x-0 w-full flex items-center justify-center px-72 py-6 space-x-3 z-10 border-t bg-white-custom'>
             <div className="flex flex-col space-y-2">
                 <button className={`font-bold px-1.5 py-1.5 rounded-md text-xs border ${buttonClicked === 'Graph' ? 'border border-blue-primary text-blue-primary' : 'border-transparent text-gray-300'}`}
                 onClick={() => {setButtonClicked('Graph'); setIsChatVisible(false);}}>Graph</button>
                 <button className={`font-bold px-1.5 py-1.5 rounded-md text-xs border ${buttonClicked === 'Chat' ? 'border border-blue-primary text-blue-primary' : 'border-transparent text-gray-300'}`}
                 onClick={() => {setButtonClicked('Chat'); setIsChatVisible(true);}}>Chat</button>
             </div>
-            {isLoading ? (
-                <div className="mb-5 h-4 overflow-hidden rounded-full bg-gray-200 w-full min-w-min">
-                    <div className="h-4 animate-pulse rounded-full bg-gradient-to-r from-green-500 to-blue-500" style={{width: '75%'}}></div>
-                </div>
-            ) : (
-                <textarea
-                ref={textareaRef}
-                rows={1}
-                className="flex-grow mr-3 px-3 py-2 rounded-md border border-gray-200 focus:border-blue-500 focus:outline-none disabled:border-gray-200 bg-white-custom"
-                placeholder="Ask me anything..."
-                onChange={(e) => setInputValue(e.target.value)}
-                value={inputValue}
-                />
-            )}
+            <textarea
+            ref={textareaRef}
+            rows={1}
+            className="flex-grow mr-3 px-3 py-2 rounded-md border border-gray-200 focus:border-blue-primary focus:outline-none disabled:border-gray-200 bg-white-custom"
+            placeholder="Ask me anything..."
+            onChange={(e) => setInputValue(e.target.value)}
+            value={isLoading ? loadingMessage : inputValue}
+            disabled={isLoading}
+            style={{ opacity: opacity, transition: 'opacity 0.5s' }}
+            />
             <button
             className={`px-2.5 py-2.5 rounded-lg bg-blue-primary text-white-custom font-bold ${inputValue ? 'opacity-100' : 'opacity-50'} disabled:opacity-50`}
             onClick={handleSubmit}
